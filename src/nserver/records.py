1fa5233868daccfffd793d428cceba2690a70dd3
Nicholas Hairs <info@nicholashairs.com>
2020-02-29 01:02:18 +1100

    Finalise Record interfaces and implement

--- src/nserver/records.py
+++ src/nserver/records.py
@@ -52,8 +52,8 @@ class A(RecordBase):  # pylint: disable=invalid-name
     """A (IPv4) Record.
     """
 
-    def __init__(self, ip: Union[str, IPv4Address], **base_kwargs):
-        super().__init__(**base_kwargs)
+    def __init__(self, name: str, ip: Union[str, IPv4Address], ttl: int = None):
+        super().__init__(name, ttl)
 
         if isinstance(ip, str):
             try:
@@ -71,8 +71,8 @@ class AAAA(RecordBase):
     """AAAA (IPv6) Record.
     """
 
-    def __init__(self, ip: Union[str, IPv6Address], **base_kwargs):
-        super().__init__(**base_kwargs)
+    def __init__(self, name: str, ip: Union[str, IPv6Address], ttl: int = None):
+        super().__init__(name, ttl)
 
         if isinstance(ip, str):
             try:
@@ -88,13 +88,19 @@ class MX(RecordBase):
     """MX Record.
     """
 
+    def __init__(self, name: str, domain: str, priority: int = 10, ttl: int = None):
+        super().__init__(name, ttl)
+
+        self._record_kwargs = {"label": domain, "preference": priority}
+        return
+
 
 class TXT(RecordBase):
     """TXT Record.
     """
 
-    def __init__(self, text: str, **base_kwargs):
-        super().__init__(**base_kwargs)
+    def __init__(self, name: str, text: str, ttl: int = None):
+        super().__init__(name, ttl)
 
         # NOTE: consider converting to bytes to allow for unicode or other.
         # Either that or enforce only ascii characters
@@ -119,10 +125,10 @@ class CNAME(RecordBase):
     """CNAME Record.
     """
 
-    regex = re.compile(r"(?:[a-z\-\_]+\.)+(?:[a-z\-\_]+)\.?")
+    regex = re.compile(r"(?:[a-z0-9\-\_]+\.)+(?:[a-z0-9\-\_]+)\.?")
 
-    def __init__(self, domain: str, **base_kwargs):
-        super().__init__(**base_kwargs)
+    def __init__(self, name: str, domain: str, ttl: int = None):
+        super().__init__(name, ttl)
 
         # TODO support converting unicode domains
         if not self.regex.fullmatch(domain):
@@ -150,9 +156,46 @@ class PTR(CNAME):
 
 class SOA(RecordBase):
     """Start of Authority Record.
+
+    See also:
+        - https://en.wikipedia.org/wiki/SOA_record#Structure
     """
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        name: str,
+        primary_name_server: str,
+        admin_email: str,
+        zone_serial: int,
+        refresh_period: int,
+        retry_period: int,
+        expires: int,
+        minimum_ttl: int,
+        ttl: int = None,
+    ):
+        super().__init__(name, ttl)
+
+        self._record_kwargs = {
+            "mname": primary_name_server,
+            "rname": admin_email,  # TODO: parse this for usability
+            "times": (zone_serial, refresh_period, retry_period, expires, minimum_ttl,),
+        }
+        return
 
 
 class SRV(RecordBase):
     """SRV Record.
     """
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self, name: str, priority: int, weight: int, port: int, target: str, ttl: int = None
+    ):
+        super().__init__(name, ttl)
+
+        self._record_kwargs = {
+            "priority": priority,
+            "weight": weight,
+            "port": port,
+            "target": target,
+        }
+        return
